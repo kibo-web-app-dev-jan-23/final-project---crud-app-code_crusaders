8258c7739bb421f896a242e1d1b7e23b45fe67ad
Sulaimon Olanrewaju <sirtunx.hokyky@gmail.com>
2023-03-08 22:55:58 +0100

    Update model file

--- models.py
+++ models.py
@@ -1,8 +1,13 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Boolean, Integer, Column, ForeignKey, String
+from flask_migrate import Migrate
 
+
+app = Flask(__name__)
 
 db = SQLAlchemy()
+migrate = Migrate(app, db)
 
 
 ProductBrand = db.Table(
@@ -22,6 +27,17 @@ class Vehicle(db.Model):
     price = db.Column(db.Float, nullable=False)
     orders = db.relationship('Order', backref='vehicle', lazy=True)
 
+    def __init__(self, make, model, year, range, price, orders):
+        self.name = make
+        self.model = model
+        self.year = year
+        self.range = range
+        self.price = price
+        self.orders = orders
+
+    def __repr__(self):
+        return f"<Vehicles {self.name}>"
+
 
 class Order(db.Model):
     __tablename__ = "orders"
@@ -31,3 +47,12 @@ class Order(db.Model):
     customer_name = db.Column(db.String(50), nullable=False)
     customer_email = db.Column(db.String(50), nullable=False)
     order_date = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, vehicle_id, customer_name, customer_email, order_date, price, orders):
+        self.vehicle_id = vehicle_id
+        self.customer_name = customer_name
+        self.customer_email = customer_email
+        self.order_date = order_date
+
+    def __repr__(self):
+        return f"<Orders {self.name}>"
